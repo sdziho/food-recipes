b199377d31c96fedc171fc155da17fa8ca44e832
sdziho <sdziho1@users.noreply.github.com>
2022-07-04 00:28:51 +0200

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'food-recipes';
+  
 }
